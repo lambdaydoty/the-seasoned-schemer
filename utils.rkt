4e6fe1f51a2aa8efb801845db07b52437eb78aa5
lambdaydoty <euphrates.tigris@gmail.com>
2019-10-13 09:48:57 +0800

    Initial commit

---
+++ utils.rkt
@@ -0,0 +1,29 @@
+#lang racket
+(provide (all-defined-out))
+
+(define (atom? x)
+  (and (not (pair? x))
+       (not (null? x))))
+
+(define (s-exp? x)
+  (or (atom? x)
+      (list? x)))
+
+(define (listing . l)
+  (begin
+    (for-each (lambda (x) (printf "~a " x)) l)
+    (newline)
+    ))
+
+(define (map fn l)
+  (cond [(null? l) '()]
+        [else (cons (fn (car l))
+                    (map fn (cdr l)))]))
+
+(define (filter p l)
+  (cond [(null? l) '()]
+        [else (if (p (car l))
+                  (cons (car l) (filter p (cdr l)))
+                  (filter p (cdr l)))]))
+
+
