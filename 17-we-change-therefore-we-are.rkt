369e74c387103a85006e77c2d7b8e6a38daf8a76
lambdaydoty <euphrates.tigris@gmail.com>
2019-11-04 21:58:32 +0800

    Refactor Chapter 17

--- 17-we-change-therefore-we-are.rkt
+++ 17-we-change-therefore-we-are.rkt
@@ -15,12 +15,14 @@
 #| (check-equal? (sweet-tooth 'chocolate) '(chocolate cake)) |#
 
 ((λ ()
+
    (define deep
      (λ (m)
        (if (zero? m)
            'pizza
            (list (deep (sub1 m))))))
    (check-equal? (deep 5) '(((((pizza))))))
+
    ))
 
 ((λ ()
@@ -28,9 +30,55 @@
    (define deepM
      (let ([Rs '()]
            [Ns '()])
+       (letrec ([D (λ (m)
+                  (if (zero? m)
+                      'pizza
+                      (list (deepM (sub1 m)))))])
+         (λ (n)
+           (let ([exists (find n Ns Rs)])
+             (if (atom? exists) ; #f
+                 (let ([result (D n)])
+                   (set! Rs (cons result Rs))
+                   (set! Ns (cons n Ns))
+                   result)
+                 exists))))))
+
+   (check-equal? (deepM 0) 'pizza)
+   (check-equal? (deepM 5) '(((((pizza))))))
+
+   ))
+
+((λ () ; remove letrec
+
+   (define deepM
+     (let ([Rs '()]
+           [Ns '()]
+           [D (λ (m)
+                (if (zero? m)
+                    'pizza
+                    (list (deepM (sub1 m)))))])
        (λ (n)
          (let ([exists (find n Ns Rs)])
-           (if (atom? exists)
+           (if (atom? exists) ; #f
+               (let ([result (D n)])
+                 (set! Rs (cons result Rs))
+                 (set! Ns (cons n Ns))
+                 result)
+               exists)))))
+
+   (check-equal? (deepM 0) 'pizza)
+   (check-equal? (deepM 5) '(((((pizza))))))
+
+   ))
+
+((λ () ; remove D
+
+   (define deepM
+     (let ([Rs '()]
+           [Ns '()])
+       (λ (n)
+         (let ([exists (find n Ns Rs)])
+           (if (atom? exists) ; #f
                (let ([result (if (zero? n)
                                  'pizza
                                  (list (deepM (sub1 n))))])
@@ -39,9 +87,6 @@
                  result)
                exists)))))
 
-   #| (trace find) |#
-   #| (trace deepM) |#
-
    (check-equal? (deepM 0) 'pizza)
    (check-equal? (deepM 5) '(((((pizza))))))
 
@@ -49,11 +94,13 @@
 
 ((λ () ; count cons
 
-   (define counter (λ () #f))
-   (define set-counter (λ () #f))
+   (define counter #f)
+
+   (define set-counter #f)
+
    (define consC
      (let ([N 0])
-       (set! counter (λ () N)) ; getter
+       (set! counter (λ () N))               ; getter
        (set! set-counter (λ (x) (set! N x))) ; setter
        (λ (x y)
          (set! N (add1 N))
@@ -77,24 +124,27 @@
    (check-equal? (deep 7) '(((((((pizza))))))))
    (check-equal? (counter) 12)
 
+   ; to determine 500,500
+
    (define supercounter
      (λ (f)
-       (letrec ([S (λ (n)
-                     (if (zero? n)
-                         (f n)
-                         (let ()
-                           (f n)
-                           (S (sub1 n)))))])
-       (S 1000)
-       (counter))))
+       (letrec
+         ([S (λ (n)
+               (if (zero? n)
+                   (f n)
+                   (let ()
+                     (f n)
+                     (S (sub1 n)))))])
+         (S 1000)
+         (counter))))
 
    (check-equal? (supercounter deep) 500512)
 
    (set-counter 0)
 
-   (check-equal? (supercounter deep) 500500)
+   (check-equal? (supercounter deep) 500500) ; 1000 + 999 + 998 + ... + 2 + 1 = 500,500
 
-   (define deepM
+   (define deepM ; inject consC into deepM
      (let ([Rs '()]
            [Ns '()])
        (λ (n)
@@ -121,30 +171,33 @@
 
    ))
 
+; rember1*
+
 ((λ ()
 
    (define rember1*
      (λ (a l)
-       (letrec ([R (λ (l oh)
-                     (match l
-                       [`() (oh 'no)]
-                       [`(,(? atom? x) ,y ...) (if (eq? (x a))
-                                                   y
-                                                   (cons x (R y oh)))]
-                       [`(,x ,y ...) (let ([new-x (let/cc oh (R x oh))])
-                                       (if (atom? new-x)
-                                           (cons x (R y oh))
-                                           (cons new-x y)))]))])
+       (letrec
+         ([R (λ (l oh)
+               (match l
+                 [`() (oh 'no)]
+                 [`(,(? atom? x) ,_l ...) (if (eq? (x a))
+                                             _l
+                                             (cons x (R _l oh)))]
+                 [`(,s ,_l ...) (let ([new-s (let/cc oh (R s oh))])
+                                 (if (atom? new-s)
+                                     (cons s (R _l oh))
+                                     (cons new-s _l)))]))])
          (let ([new-l (let/cc oh (R l oh))])
            (if (atom? new-l)
                l
                new-l)))))
 
-   (define counter (λ () #f))
-   (define set-counter (λ () #f))
+   (define counter #f)
+   (define set-counter #f)
    (define consC
      (let ([N 0])
-       (set! counter (λ () N)) ; getter
+       (set! counter (λ () N))              ; getter
        (set! set-counter (λ (x) (set! N x))) ; setter
        (λ (x y)
          (set! N (add1 N))
@@ -155,13 +208,13 @@
        (letrec ([R (λ (l oh)
                      (match l
                        [`() (oh 'no)]
-                       [`(,(? atom? x) ,y ...) (if (eq? x a)
-                                                   y
-                                                   (consC x (R y oh)))]
-                       [`(,x ,y ...) (let ([new-x (let/cc oh (R x oh))])
+                       [`(,(? atom? x) ,_l ...) (if (eq? x a)
+                                                   _l
+                                                   (consC x (R _l oh)))]
+                       [`(,s ,_l ...) (let ([new-x (let/cc oh (R s oh))])
                                        (if (atom? new-x)
-                                           (consC x (R y oh))
-                                           (consC new-x y)))]))])
+                                           (consC s (R _l oh))
+                                           (consC new-x _l)))]))])
          #| (trace R) |#
          (let ([new-l (let/cc oh (R l oh))])
            (if (atom? new-l)
@@ -175,27 +228,28 @@
 
    (define rember1*C2
      (λ (a l)
-       (letrec ([R (λ (l)
-                     (match l
-                       [`() '()]
-                       [`(,(? atom? x) ,y ...) (if (eq? x a)
-                                                   y
-                                                   (consC x (R y)))]
-                       [`(,x ,y ...) (let ([av (R x)])
-                                       (if (equal? x av)
-                                           (consC x (R y))
-                                           (consC av y)))]))])
+       (letrec
+         ([R (λ (l)
+               (match l
+                 [`() '()]
+                 [`(,(? atom? x) ,_l ...) (if (eq? x a)
+                                             _l
+                                             (consC x (R _l)))]
+                 [`(,s ,_l ...) (let ([av (R s)])
+                                 (if (equal? s av)
+                                     (consC s (R _l))
+                                     (consC av _l)))]))])
          (R l))))
 
    (set-counter 0)
 
    (let ([f 'food]
          [m 'more])
-         (check-equal? (consC (consC f '())
-                              (consC m
-                                     (consC (consC f '())
-                                            '())))
-                       '((food) more (food))))
+     (check-equal? (consC (consC f '())
+                          (consC m
+                                 (consC (consC f '())
+                                        '())))
+                   '((food) more (food))))
 
    (check-equal? (counter) 5)
 
